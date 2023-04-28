32bad66fef734b6bd446cb10fe4c9ec342202ac1
ercee <ercetilav@gmail.com>
2023-04-29 01:53:26 +0200

    dockerize

--- src/main/java/com/example/xmcyodc/util/CsvUtility.java
+++ src/main/java/com/example/xmcyodc/util/CsvUtility.java
@@ -7,10 +7,13 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +27,11 @@ public class CsvUtility {
 
 
     private static CsvMapper mapper;
+    private final ResourceLoader resourceLoader;
+
+    public CsvUtility(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     public static CsvMapper getMapper() {
         if (mapper == null) {
@@ -36,26 +44,30 @@ public class CsvUtility {
 
     public List<String> listCsvFiles() throws IOException {
         List<String> csvFileNames = new ArrayList<>();
-
-        ClassPathResource resource = new ClassPathResource(csvFolderPath);
-        File folder = resource.getFile();
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles((dir, name) -> name.endsWith(".csv"));
-            for (File file : files) {
-                csvFileNames.add(file.getName());
-            }
+        Resource[] resources = ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
+                .getResources(csvFolderPath + "/*.csv");
+        for (Resource resource : resources) {
+            csvFileNames.add(resource.getFilename());
         }
         return csvFileNames;
     }
 
+
     public List<CryptoCurrency> readCsvFileOfCrypto(String fileName) throws IOException {
         CsvMapper mapper = getMapper();
-        mapper.registerModule(new JavaTimeModule());
         CsvSchema schema = mapper.schemaFor(CryptoCurrency.class).withHeader();
-        ClassPathResource resource = new ClassPathResource(csvFolderPath + "/" + fileName.toUpperCase(Locale.US) + "_values.csv");
+        Resource resource = resourceLoader
+                .getResource(csvFolderPath + "/" + fileName.toUpperCase(Locale.US) + "_values.csv");
+        if (!resource.exists()) {
+            throw new FileNotFoundException("Resource " + fileName + " does not exist");
+        }
+        if (!resource.isReadable()) {
+            throw new IOException("Resource " + fileName + " is not readable");
+        }
         File file = resource.getFile();
         MappingIterator<CryptoCurrency> iterator = mapper.readerFor(CryptoCurrency.class)
                 .with(schema).readValues(file);
         return iterator.readAll();
     }
+
 }
